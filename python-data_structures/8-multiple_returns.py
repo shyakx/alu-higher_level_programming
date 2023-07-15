adcb82fd43307011c53d0ff5f83857bbdd83426f
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-15 08:06:53 -0700

    6-print_matrix_integer.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,13 +3,4 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
-    return length, first_char
-
-# Example usage:
-sentence = "Hello, World!"
-result_tuple = multiple_returns(sentence)
-print(result_tuple)  # Output: (13, 'H')
-
-empty_sentence = ""
-result_tuple_empty = multiple_returns(empty_sentence)
-print(result_tuple_empty)  # Output: (0, None)
+    return (length, first_char)
