548e85e499476d35308248ef7bfdece53629944e
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-22 10:44:16 -0700

    new changes on this file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,19 +6,29 @@ class Square:
     This class represents a square.
 
     Attributes:
-        None
-
-    Methods:
-        None
+        dict_: dict, a dictionary to store square properties.
+              The dictionary contains the following key-value pairs:
+              - 'side_length': float, the length of each side of the square.
+              - 'area': float, the area of the square calculated as side_length ** 2.
     """
     def __init__(self):
         """
-        Initializes a Square object.
-        """
-        self.dict_ = {}
+        Initializes a Square instance.
 
-# Test case
+        Parameters:
+            None
+        """
+        self.dict_ = {}  # Initialize an empty dictionary for square properties
+
+# Test the Square class
 if __name__ == "__main__":
+
+    """
+    This instance allows us to control
+    the pthon script at the execution
+    """
+
     mysquare = Square()
-    print(type(mysquare))  # Output: <class '__main__.Square'>
-    print(mysquare.dict_)  # Output: {}
+    print(type(mysquare))
+    print(mysquare.dict_)
+
