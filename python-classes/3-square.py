379eac99563f02212dd024599c3a1951a2ebd70d
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-24 05:16:27 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,17 +4,18 @@
 
 class Square:
     '''Initializing a special method __init__'''
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         ''' giving a condition to check the if size is integer '''
         if not isinstance(size, int):
             ''' here it will raise a type error if size is not integer '''
             raise TypeError("size must be an integer")
-        ''' giving a condition to check if size if negative and it raise an error'''
+        ''' giving a condition to check if size if negative'''
         if size < 0:
             ''' here it will raise ValueError '''
             raise ValueError("size must be >= 0")
         ''' variable creation '''
         self.__size = size
+    
     def area(self):
         return self.__size ** 2
     ''' getter statement to get a value '''
