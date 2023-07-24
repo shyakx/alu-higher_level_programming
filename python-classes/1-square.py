bd31ad45304ab4ccf221101d5d4b5598b523ad71
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-24 04:00:53 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''creating a class called Square'''
+class Square:
+    '''Initializing a specialmethon __init__ which contains a private attribute size'''
+    def __init__(self,size):
+        '''Creating a new variable'''
+        self._size = size
