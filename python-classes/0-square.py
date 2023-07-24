adc6d7d5576daf1eac5a8da0f58ac056fe5d7e72
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-24 03:51:03 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,8 +4,5 @@
 
 
 class Square:
-
-    '''initializing a special method __init__ '''
-    def __init__(self, name):
-        ''' creating a new variable'''
-        self._name = name
+    ''' here is an empty class'''
+    pass
