c00f472697e13c17cad5a72be5a3ca7b6249819b
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-24 05:25:59 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,9 +15,10 @@ class Square:
             raise ValueError("size must be >= 0")
         ''' variable creation '''
         self.__size = size
-    
+
     def area(self):
         return self.__size ** 2
-    ''' getter statement to get a value '''
-    def get_size(self):
+
+    '''Getter statement to get a value '''
+    def get_size(size):
         return self.__size
