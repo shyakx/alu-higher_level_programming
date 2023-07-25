c664f700101da6c5f439fd76e3dd3862c4eadea2
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-25 02:18:46 -0700

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+class creation
+'''
+
+
+class Rectangle:
+    '''
+    no arguments
+    no methods
+    '''
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+my readme file for python-more_classes project
