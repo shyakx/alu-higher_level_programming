dd7116b4854245af6e082684c0fb381b8e0a83ad
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-25 03:18:32 -0700

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,9 +22,9 @@ class Rectangle:
     private instance width setter
     '''
     def width(self, value):
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
@@ -39,9 +39,9 @@ class Rectangle:
     private instance heifht setter
     '''
     def height(self, value):
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
 
         self.__height = value
