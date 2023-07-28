d905806eeb346a90b5bc7d1f491e89e6b63990c9
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-28 05:57:11 -0700

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 '''
-create a subclass that inherits from list class
+create a subclass that inherits from list 
+class
 '''
 
-
 class MyList(list):
+    """
+    MyList class is a subclass of list that allows printing the list in sorted (ascending) order.
+
+    Public instance method:
+    - print_sorted(self): Prints the list in ascending order (sorted).
+    """
     def print_sorted(self):
         """
         Prints the list in ascending order (sorted).
         """
         sorted_list = sorted(self)
         print(sorted_list)
+
