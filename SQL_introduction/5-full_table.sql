984d550f1e284c809c95ea80f8690cead7ef084d
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-08-01 00:57:03 -0700

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,37 +1,4 @@
--- this is a script that prints full description of table 'first_table' from hbtn_0c_0database
-SET @table_name = 'first_table';
-SET @database_name = 'hbtn_0c_0';
-
-SELECT
-    CONCAT(
-        'CREATE TABLE `', @table_name, '` (',
-        GROUP_CONCAT(
-            CONCAT('`', COLUMN_NAME, '` ', COLUMN_TYPE,
-            IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-            IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', IFNULL(QUOTE(COLUMN_DEFAULT), 'NULL')), ''),
-            IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
-            ',')
-            ORDER BY ORDINAL_POSITION
-        ),
-        IFNULL(
-            CONCAT(
-                ', PRIMARY KEY (',
-                GROUP_CONCAT(
-                    IF(COLUMN_KEY = 'PRI', CONCAT('`', COLUMN_NAME, '`'), NULL)
-                    ORDER BY ORDINAL_POSITION
-                ),
-                ')'
-            ),
-            ''
-        ),
-        ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME
-    ) AS CreateTableStatement
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = @database_name
-    AND TABLE_NAME = @table_name
-GROUP BY
-    TABLE_NAME,
-    ENGINE,
-    CHARACTER_SET_NAME;
+-- this is a script to print the full description of first_table
+SELECT *
+FROM hbtn_0c_0
+WHERE table_name = first_table
