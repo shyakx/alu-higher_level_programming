8fcab353bfe76576eabd4421339089e5ebc7ba70
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-31 23:12:39 -0700

    4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 --This is a script to create a table called`first_name`
 
-CREATE TABLE IF NOT EXISTS `hbtn_test_db_4`.`first_table` (
-  `id` INT,
-  `name` VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
 );
