3a1d8cacdd7ce2129792d377798c12c439625793
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-08-01 01:23:23 -0700

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- this is a script to print the full description of first_table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+AND TABLE_NAME = 'first_table'
