8cd1cbfc96f63579369db807e21395833ef76187
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-08-01 01:52:14 -0700

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- this is a script to print the full description of first_table
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table'
+SELECT * hbtn_0c_0.first_table 
