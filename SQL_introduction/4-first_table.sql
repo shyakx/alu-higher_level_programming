06f70c0210b6491e52b0ff960132e139c2eb0f00
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-31 22:59:19 -0700

    4-first_table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 --This is a script to create a table with id and name attributes
 
-CREATE TABLE IF NOT EXISTS `first_name`(
+CREATE TABLE IF NOT EXISTS `mysql`.`first_name`(
 	`id` INT,
 	`name` VARCHAR(256)
 );
