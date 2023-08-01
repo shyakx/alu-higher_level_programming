ee7dc03550029c47fda284738a21b30ea44e9101
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-07-31 23:48:27 -0700

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- the script that shows all descriptionsof a table in a database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table;
