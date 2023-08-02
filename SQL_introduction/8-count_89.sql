fb686f9de2d0dd5e038a2911dc9ebbf8d0a8b3f3
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-08-02 02:58:31 -0700

    8-count_89.sql

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
 --This is a script that displays the number of records
-SELECT COUNT (*) FROM `first_table` WHERE id=89;
+--Where The database name will be passed as an argument of the mysql command
+SELECT COUNT(*) FROM `first_table` WHERE `id` = 89;
