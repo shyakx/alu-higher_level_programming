2b757b8faf95f9d589d2003a8699cb4a364c0d33
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-08-02 02:45:18 -0700

    8-count_89.sql

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,2 @@
 --This is a script that displays the number of records
-SELECT COUNT * AS TotalRecords
-FROM hbtn_0c_0.first_table
-WHERE id = 89;
+SELECT COUNT (*) FROM `first_table` WHERE `id`= 89;
