d9a64c2cea5efac64f3dd676078d34462b4664b3
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-09-30 11:22:44 -0700

    1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let var1 = "C is fun"
-let var2 = "Python is cool"
-let var3 = "JavaScript is amazing"
+let var1 = "C is fun";
+let var2 = "Python is cool";
+let var3 = "JavaScript is amazing";
 
 console.log(var1)
 console.log(var2)
