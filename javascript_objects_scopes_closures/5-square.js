f7acdb393d9de484bcdfc129c937041898d590e0
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-09-30 13:48:25 -0700

    5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,32 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rectangle = require('./4-rectangle');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
-    }
-  }
-
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
 
-module.exports = Rectangle;
+module.exports = Square;
