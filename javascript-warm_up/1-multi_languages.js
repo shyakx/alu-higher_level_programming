e47fa826f1fc5b23e9ee516db09be39b7d0d5400
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-09-30 11:35:18 -0700

    0-javascript_is_amazing.js 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -4,6 +4,6 @@ let var1 = "C is fun";
 let var2 = "Python is cool";
 let var3 = "JavaScript is amazing";
 
-console.log(var1)
-console.log(var2)
-console.log(var3)
+console.log(var1);
+console.log(var2);
+console.log(var3);
